ac41a893e2c6545a8e60978106739f00489e06a3
Hakan <hakanyildiz248@gmail.com>
2019-08-04 20:09:47 +0300

    Navbar created and New Bootcamp button added !

---
+++ src/components/Header/NewBootcamp/searchAppBar.js
@@ -0,0 +1,116 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import { Button } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import { opaqueType } from "@babel/types";
+import { fontFamily } from "@material-ui/system";
+import { MemoryRouter as Router } from "react-router";
+import { Link as RouterLink } from "react-router-dom";
+import Link from "@material-ui/core/Link";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    opacity: 0.8,
+    backgroundColor: "#FF9C53",
+    borderColor: "#FF9C53",
+    boxShadow: "0 0 3px  orange",
+    sticky: "top"
+  },
+  title: {
+    flexGrow: 1,
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit"
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: 120,
+      "&:focus": {
+        width: 200
+      }
+    }
+  }
+}));
+
+export default function SearchAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position="static"
+        style={{ backgroundColor: "#FF9C53", borderColor: "#FF9C53" }}
+      >
+        <Toolbar>
+          <Typography className={classes.title} variant="h4" noWrap>
+            urBootcamp !
+          </Typography>
+          <div className="sectionLinks" />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <div>
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "#97A067",
+                opacity: 0.8,
+                color: "white",
+                marginLeft: 25
+              }}
+            >
+              New Bootcamp
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
