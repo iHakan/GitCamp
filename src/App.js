efcc361daaeeb27fe1dbdbbf061730be959db739
Hakan <hakanyildiz248@gmail.com>
2019-08-06 19:22:13 +0300

    Removing of the Router

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <SearchAppBar />
+      {/*Buraya tüm sectionların taglerini oluşturup koyucam ki router çalışsın! */}
     </div>
   );
 }
