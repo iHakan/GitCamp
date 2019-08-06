e361bafd315f819f25bb916656091d5ab792400a
Arif <arifbaglar@hotmail.com>
2019-08-06 19:26:14 +0300

    footer and contact added
    footer and contact file added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import SearchAppBar from "./components/Header/NewBootcamp/searchAppBar";
+import Footer from "./components/Footer/footer";
+import Contactus from "./components/Footer/contactus";
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar />
+      <Contactus />
+      <Footer />
     </div>
   );
 }
