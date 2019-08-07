6b722ad2d204fe807e3acb76b91faf95905dfdcd
Hakan <hakanyildiz248@gmail.com>
2019-08-08 00:19:36 +0300

    Some additions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import SearchAppBar from "./components/Header/NewBootcamp/searchAppBar";
-import Map from "./components/Map"
+import Map from "./components/Map";
+import Carousel from "./components/Upcomings";
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar />
-      <Map/>
+      <Carousel />
+      <Map />
       {/*Buraya tüm sectionların taglerini oluşturup koyucam ki router çalışsın! */}
     </div>
   );
